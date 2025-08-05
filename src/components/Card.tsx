0d61ea05d9ef4c5135261020db4d57ccbe3a3af3
Samuel Melo <samuelmelo13789@gmail.com>
2025-08-04 21:29:29 -0300

    Criação do catálogo de aves e plantas Ativ/part2

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,6 +4,7 @@ interface CardProps {
     title: string;
     description: string;
     imageUrl: string
+    category: string
 }
 
 export default function Card(prop : CardProps) {
@@ -13,23 +14,30 @@ export default function Card(prop : CardProps) {
         <div className="w-90 h-fit bg-[#9ac254] rounded-[15px] text-white shadow-lg shadow-black/50">     
 
             <div className="flex justify-center items-center py-4 bg-[#81a249] rounded-t-[15px]">
-                <p className="font-bold text-[18px] break-words">{prop.title}</p>
+                <p className="font-bold text-[18px] break-words leading-5">{prop.title}</p>
             </div>
 
             <div className="flex justify-start items-center px-5 mt-2 ">
 
-                <p className="break-words"> {prop.description} </p>
+                <p className="break-words leading-5"> <strong> Descrição:  </strong> {prop.description} </p>
                 
             </div>
 
-            <div className="flex justify-center items-center py-4">
-                <div className="w-[90%] h-50 relative">
+            <div className="flex justify-start items-center px-5">
+
+                <p>  <strong> Categoria:  </strong> {prop.category} </p>
+
+            </div>
+            
+            <div className="flex  flex-col justify-center items-center mb-4 mt-2">
+                <span className="mb-2"> <strong> Imagem:  </strong> </span>
+                <div className="w-[70%] h-70 relative">
                     <Image
 
                         src={prop.imageUrl}
                         alt="Alguma imagem"
                         fill
-                        className="object-cover rounded-2xl justify-center"
+                        className="object-cover object-center rounded-2xl justify-center"
                     />
                 </div>
             </div>
