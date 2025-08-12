229692645f98afa1913677b8af28724888ec8fd2
Samuel Melo <samuelmelo13789@gmail.com>
2025-08-12 18:29:26 -0300

    Ajustes e alterações de animação

--- src/app/animations/page.tsx
+++ src/app/animations/page.tsx
@@ -7,24 +7,42 @@ export default function AnimationsPage() {
 
     useEffect(() => {
 
-        gsap.fromTo(".box1", {x: 100, opacity: 0.0}, {x: 300, opacity:1, duration: 1})
-        gsap.to(".box2", {rotation: 360, x:200, duration: 2})
-        gsap.from(".box3", {rotation: 360 , y: -200, duration: 2})
+        
 
     }, [])
     
     
     return (
 
-        <div className="w-full h-screen flex justify-start items-center px-20">
+        <div className="w-full h-screen flex justify-start items-center px-20 flex-col gap-10 py-10">
 
             <div className="flex justify-start items-center flex-col gap-15">
 
-                <div className="box1 h-20 w-20 bg-green-500 rounded-[15px]"></div>
+                <div className="box1 h-50 w-100 bg-green-500 rounded-[15px]"></div>
 
-                <div className="box2 h-20 w-20 bg-blue-300 rounded-[15px]"></div>
+                <div className="box2 h-50 w-100 bg-blue-300 rounded-[15px]"></div>
 
-                <div className="box3 h-20 w-20 bg-red-500 rounded-[15px]"></div>
+                <div className="box3 h-50 w-100 bg-red-500 rounded-[15px]"></div>
+
+            </div>
+
+            <div className="flex justify-start items-center flex-col gap-15">
+
+                <div className="box4 h-50 w-100 bg-green-500 rounded-[15px]"></div>
+
+                <div className="box5 h-50 w-100 bg-blue-300 rounded-[15px]"></div>
+
+                <div className="box6 h-50 w-100 bg-red-500 rounded-[15px]"></div>
+
+            </div>
+
+            <div className="flex justify-start items-center flex-col gap-15">
+
+                <div className="box7 h-50 w-100 bg-green-500 rounded-[15px]"></div>
+
+                <div className="box8 h-50 w-100 bg-blue-300 rounded-[15px]"></div>
+
+                <div className="box9 h-50 w-100 bg-red-500 rounded-[15px]"></div>
 
             </div>
 
