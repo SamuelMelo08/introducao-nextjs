a7bc670a5bf4e07c9d92cac00c2a05f1a7f2366d
Samuel Melo <samuelmelo13789@gmail.com>
2025-08-14 18:50:23 -0300

    Conclusão da calculadora

--- src/components/MiniDisplay.tsx
+++ src/components/MiniDisplay.tsx
@@ -6,11 +6,91 @@ import { useState } from "react"
 
 export default function MiniDisplay() {
 
-    const [display, setDisplay] = useState<string>("0")
+    const [display, setDisplay] = useState<string>("")
+
+    const operationSearch = () => {
+
+        if (display.includes("+")) {
+
+            if (display.includes("-") || display.includes("x") || display.includes("/")) {
+                return
+            }
+
+            sum()
+
+        } else if (display.includes("-")) {
+
+            if (display.includes("+") || display.includes("x") || display.includes("/")) {
+                return
+            }
+
+            minos()
+
+        } else if (display.includes("x")) {
+
+            if (display.includes("-") || display.includes("+") || display.includes("/")) {
+                return
+            }
+
+            multi()
+
+        } else if (display.includes("/")) {
+
+            if (display.includes("-") || display.includes("x") || display.includes("+")) {
+                return
+            }
+
+            div()
+        }
+    }
 
     const deleteNumber = () => {
 
         setDisplay(display.slice(0, -1))        
+
+    }
+
+    const sum = () => {
+
+        const values = display.split("+")
+        
+        const result = values.reduce((acumulation, currentValue) =>  acumulation + parseFloat(currentValue), parseFloat(values[0]) )
+
+        setDisplay(`${result}`)
+
+    }
+
+    const minos = () => {
+
+        const values = display.split("-")
+        const numberValues = values.map((value) => parseFloat(value))
+
+        const result = numberValues.reduce((acumulation, currentValue) =>  acumulation - currentValue)
+
+
+        setDisplay(`${result}`)
+
+    }
+
+    const multi = () => {
+
+        const values = display.split("x")
+        const numberValues = values.map((value) => parseFloat(value))
+
+        const result = numberValues.reduce((acumulation, currentValue) =>  acumulation * currentValue)
+
+        setDisplay(`${result}`)
+
+    }
+
+    const div = () => {
+
+        const values = display.split("/")
+        const numberValues = values.map((value) => parseFloat(value))
+
+        const result = numberValues.reduce((acumulation, currentValue) =>  acumulation / currentValue)
+
+        setDisplay(`${result}`)
 
     }
 
@@ -19,14 +99,14 @@ export default function MiniDisplay() {
         <div className="flex flex-col justify-center items-center mt-5 bg-[#eddfc8] p-3 rounded">
 
             {/* Display */}
-            <div className="border-1 bg-[#bbd681] border-gray-400 rounded px-2  w-40 h-12 text-center text-xl flex justify-start items-center overflow-x-auto ">
+            <div className="border-1 bg-[#bbd681] border-gray-400 rounded p-2  w-40 h-12 text-center text-xl flex justify-start items-center overflow-x-auto overflow-y-hidden">
 
                 {display}
 
             </div>
 
             {/* Teclado númerico */}
-            <div className="grid grid-cols-3 gap-2 mt-2 w-40 h-fit ">
+            <div className="grid grid-cols-4 gap-2 mt-2 w-40 h-fit ">
 
                 <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display + "0")}>
                     0
@@ -38,6 +118,10 @@ export default function MiniDisplay() {
 
                 <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display + "2")}>
                     2
+                </button>
+
+                <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display + "+")}>
+                    +
                 </button>
 
                 <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display + "3")}>
@@ -52,6 +136,10 @@ export default function MiniDisplay() {
                     5
                 </button>
 
+                <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display + "-")}>
+                    -
+                </button>
+
                 <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display +  "6")}>
                     6
                 </button>
@@ -64,12 +152,30 @@ export default function MiniDisplay() {
                     8
                 </button>
 
+                <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display + "x")}>
+                    x
+                </button>
+
                 <button className="bg-[#434336] text-white p-2 rounded" onClick={() => setDisplay(display + "9")}>
                     9
                 </button>
 
+
+                <button className="bg-[#434336] text-white p-2 rounded col-span-2" onClick={() => operationSearch()}>
+                    =
+                </button>
+
+                <button className="bg-[#434336] text-white p-2 rounded " onClick={() => setDisplay(display + "/")}>
+                    /
+                </button>
+
                 <button className="bg-[#da4949] text-white p-2 rounded col-span-2 flex justify-center items-center" onClick={() => deleteNumber()}>
                     <FaTrash color='white' />
+                </button>
+
+
+                <button className="bg-[#da4949] text-white p-2 rounded col-span-2 " onClick={() => setDisplay("")}>
+                    C
                 </button>
 
             </div>
